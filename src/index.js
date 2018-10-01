6866ac0756d1dabf0de4544c2623426308b29c93
andreiyasinski <andrewtopol856@gmail.com>
2018-10-02 00:38:53 +0300

    almost done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let num = number;
+  let result = 0;
+
+  let divider = getDivider(base);
+
+  function getDivider(numeralSystem) {
+
+    let n = numeralSystem;
+
+    for ( let a = 2; a <= n; a++) {
+
+      for (let i = 2; i < n; i++) {
+
+        if(n % i == 0 && n > 1) {
+          n /= i;
+          break;
+        }
+
+      }
+
+    }
+    //console.log(n);
+    return n;
+  }
+
+
+  while(num > 0)
+  {
+    num /= divider;
+    result += Math.floor(num);
+  }
+
+  return result;
 }
