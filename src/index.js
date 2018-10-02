6bc6757e03fd69f9e9ff50c6f9f00952926c8f07
andreiyasinski <andrewtopol856@gmail.com>
2018-10-02 22:03:11 +0300

    almost done

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ module.exports = function getZerosCount(number, base) {
 
       for (let i = 2; i < n; i++) {
 
-        if(n % i == 0 && n > 1) {
+        if(n % i == 0) {
           n /= i;
           break;
         }
